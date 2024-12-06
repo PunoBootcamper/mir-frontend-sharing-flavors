26da744fb3d180e41b17b3432ed7894a0546ab76
William Lupaca <wlupacat@gmail.com>
2024-12-06 12:17:08 -0500

    feat: add not found route

--- src/app/routes/MainRouter.tsx
+++ src/app/routes/MainRouter.tsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import { Home, Login, Register, Chat } from "../../pages";
 import AddRecipe from "../../pages/AddRecipe";
 import Recipe from "../../pages/Recipe";
+import NotFound from "../../pages/NotFound";
 import Layout from "../../componentes/layouts/Layout";
 const MainRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
         <Route path="/recipe/:id" element={<Recipe />} />
         <Route path="/home" element={<Home />} />
       </Route>
@@ -17,6 +19,7 @@ const MainRouter = () => {
       <Route path="/add-recipe" element={<AddRecipe />} />
       <Route path="/recipe/:id" element={<Recipe />} />
       <Route path="/chat" element={<Chat />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
