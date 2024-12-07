8bd46be9bad32cc96f40f62b6964888b9136ade9
GabrielMaQui <i202217427@cibertec.edu.pe>
2024-12-06 23:11:54 -0500

    feat: implement component custom panel for user profile

---
+++ src/componentes/profile/CustomTabPanel.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+export const CustomTabPanel = (props: TabPanelProps) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+      className="userProfile__recipesTab-panel"
+    >
+      {value === index && (
+        <Box>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
