14bd7d509610808dd132baed4a9d0a4b5bf7c923
William Lupaca Ticona <wlupacat@gmail.com>
2024-12-09 08:59:26 -0500

    fix: hide favorites in recipe card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,20 +4,11 @@ import AddButton from "../componentes/ui/Button/AddButton";
 export default function Home() {
   const { data: recipes } = useGetRecipesQuery();
 
-  const localStorageData = localStorage.getItem("user");
-  const parsedData = localStorageData ? JSON.parse(localStorageData) : null;
-
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 place-items-center justify-center">
         {recipes?.map((recipe) => {
-          //const isFavorite = !!dataLoggedUser?.favorites?.includes(recipe._id);
-          const isFavorite = !!parsedData.profile.favorites.includes(
-            recipe._id,
-          );
-          return (
-            <RecipeCard key={recipe._id} {...recipe} isFavorite={isFavorite} />
-          );
+          return <RecipeCard key={recipe._id} {...recipe} />;
         })}
       </div>
 
