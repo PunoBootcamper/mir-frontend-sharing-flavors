f3f11214c69f5596faec392ebfb05fa1df40ca46
William Lupaca Ticona <wlupacat@gmail.com>
2024-12-10 00:24:55 -0500

    feat: add style rating

--- src/componentes/recipe/Stars.tsx
+++ src/componentes/recipe/Stars.tsx
@@ -26,7 +26,7 @@ const Stars = ({ rating }: StarsProps) => {
         ))}
       </div>
       <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-        {roundedRating}
+        {roundedRating > 0 ? roundedRating : "Sin rating"}
       </span>
     </div>
   );
